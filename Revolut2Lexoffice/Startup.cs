b7c612a77132eec0751caa70ae5582a26e9fd687
Michael Nikolaus <michael.nikolaus@gmail.com>
2021-09-01 23:04:57 +0200

    Cleanup

--- Revolut2Lexoffice/Startup.cs
+++ Revolut2Lexoffice/Startup.cs
@@ -1,16 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Revolut2LexOffice;
 
@@ -40,7 +34,6 @@ namespace WebApplication
 					"REVOLT21"
 				)
 			);
-
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
